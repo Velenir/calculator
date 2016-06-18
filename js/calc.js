0fec9bff1ab8b9e8b79b4ae442a77bc9c9378ef3
Velenir <Velenir@users.noreply.github.com>
2016-06-18 23:23:35 +0300

    Fixed edge cases when ans=Infinity

--- js/calc.js
+++ js/calc.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	const $display = $('#display');
 	let ans = "", toClear = false;
 
-	const transformativeValues = ["AC", "CE", "Ans", "="];
+	const transformativeValues = ["AC", "CE", "="];
 	function transformOutput(val) {
 		let res = $display.val();
 		switch (val) {
@@ -17,11 +17,11 @@ $(document).ready(function() {
 			// accept resulting output as is, even if directly after "="
 			toClear = false;
 			break;
-		case "Ans":
-			// reinstate ans if directly after "=", otherwise append to output
-			res = toClear ? ans : res + ans;
-			toClear = false;
-			break;
+		// case "Ans":
+		// 	// reinstate ans if directly after "=", otherwise append to output
+		// 	res = toClear ? ans : res + ans;
+		// 	toClear = false;
+		// 	break;
 		case "=":
 			res = evaluate(res);
 			break;
@@ -51,6 +51,7 @@ $(document).ready(function() {
 	$('.buttonPanel').on('click', 'button', function () {
 		console.log("button", this.value, "clicked");
 		console.log("toClear", toClear);
+		console.log("Answer", ans);
 		$display.parent().removeClass("evalled");
 
 
@@ -60,14 +61,17 @@ $(document).ready(function() {
 		} else {
 			if(toClear) $display.val('');
 			// const newVal = toClear ? this.value : $display.val() + this.value;
-			const newVal = $display.val() + this.value;
+			const newVal = $display.val() + (this.value === "Ans" ? ans : this.value);
+
+			// don't do any checks for "NaN", let it fail and be caught
 
 			// add new value only if eval is possible or will be possible later
 			try {
-				eval(newVal+"0");
+				console.log("trying", newVal.endsWith("Infinity") ? newVal : newVal+"0");
+				// don't append "0" to "Infinity" => always throws
+				eval(newVal.endsWith("Infinity") ? newVal : newVal+"0");
 			} catch (e) {
 				console.log("Error", e);
-				// toClear = false;
 				return;
 			}
 
